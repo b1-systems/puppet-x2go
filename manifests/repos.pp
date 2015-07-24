de3ac43fc3b772f038ca5aa2cb314927c84d1261
Mattias Giese <giese@b1-systems.de>
2015-07-24 10:56:36 +0200

    not ready yet

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -7,11 +7,44 @@
 # [*ensure*]
 #   
 class x2go::repos(
-  $ensure         = 'present',
-  $nightly_builds = false,
-  $extra_packages = false
+  $ensure           = 'present',
+  $upstream_repos   = true,
+  $nightly_builds   = false,
+  $current_packages = true,
+  $el_repofile_path = '/etc/yum.repos.d/x2go.repo',
+  $ubuntu_ppa       = 'ppa:x2go/stable',
+  $extra_packages   = false
 ) {
   validate_bool($nightly_builds)
   validate_bool($extra_packages)
+  validate_bool($upstream_repos)
+  validate_bool($current_packages)
+  validate_absolute_path($el_repofile_path)
   validate_string($ensure,'^(present|absent)','ensure must be either present or absent')
+  
+  case $ensure {
+    'present': {
+      $epel_enabled = true
+    }
+    'absent': {
+      $epel_enabled = false
+    }
+  }
+  if $::osfamily == 'RedHat' {
+    if ! defined(Class['epel']) {
+      class{'epel':
+        epel_enabled => $epel_enabled,
+      }
+      if $upstream_repos {
+        file{$el_repofile_path:
+          ensure  => $ensure,
+          content => template('x2go/x2go-rhel.repo.erb'),
+          require => Class['epel'],
+        }
+      }
+    }
+  }
+  if $::operatingsystem == 'Ubuntu' {
+    apt::ppa{$ubuntu_ppa:}
+  }
 }
